d2b76c2ddbf9d3005884f39ddf43299d4296f9bf
Kavya <abc.gmail.com>
2021-07-23 22:26:45 -0400

    initial commit

---
+++ Project1/src/com/training/basics/Hello.java
@@ -0,0 +1,16 @@
+package com.training.basics;
+
+public class Hello {
+    public static void main(String args[])
+    {
+        System.out.println("**********      **********");
+        System.out.println("**        **    **        **");
+        System.out.println("**         **   **         **");
+        System.out.println("**        **    **          **");
+        System.out.println("*********       **          **");
+        System.out.println("**        **    **          **");
+        System.out.println("**         **   **         **");
+        System.out.println("**        **    **        **");
+        System.out.println("**********      **********");
+    }
+}
